a0f7781905a30972c02e0f2bb104cae0da797ac6
NatalyShav <shaverinanatalia@gmail.com>
2025-04-30 14:50:02 +0300

    add new project

---
+++ project/models.py
@@ -0,0 +1,13 @@
+# models.py
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
